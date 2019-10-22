04c27d2e98de72ee27a14fd7ffd1fc3422f9255e
Quang Nguyen <quangn2595@gmail.com>
2019-10-22 10:52:52 -0700

    finished building the layout for the home page, add task page, and all task page. Also updated README to reflect work done today

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # TaskMaster
 
-This android app will track all the tasks that a user need to do
+This android app will track all the tasks that a user need to do and allow user to add new tasks
 
 # Daily Change Log
 
 ## Tuesday 10/22/2019 
 
-### 
+### What I Worked On
+
+* Created Home Page to include a placeholder image and two buttons to take user to add task page and all tasks page
+* For add task page, I created two input fields for user to add task title and description and also a button for submission. For now, the submit button only show a pop up message with the word "Submitted!"
+* For all tasks page, there is an image and a label.  
+
+### Images
+
+![Home Page](screenshots/home_page.png)
+
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void redirectToAddTaskActivity(View view) {
+        Intent addTaskIntent = new Intent(MainActivity.this, AddTask.class);
+        MainActivity.this.startActivity(addTaskIntent);
+    }
+
+    public void redirectToAllTaskActivity(View view) {
+        Intent allTasksIntent = new Intent(MainActivity.this, AllTasks.class);
+        MainActivity.this.startActivity(allTasksIntent);
     }
 }
